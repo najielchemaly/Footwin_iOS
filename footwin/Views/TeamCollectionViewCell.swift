30456443853b648488e6854e4408392ac5da732c
NajiElChemaly <naji.chemaly@intouchmena.com>
2018-04-11 10:04:41 +0300

    create initial screens

---
+++ footwin/Views/TeamCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TeamCollectionViewCell.swift
+//  footwin
+//
+//  Created by MR.CHEMALY on 4/11/18.
+//  Copyright © 2018 we-devapp. All rights reserved.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
